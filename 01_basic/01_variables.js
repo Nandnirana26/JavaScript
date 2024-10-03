3ca40405e8d0e3d8a80f37dc4a8df55aa00e7c25
Nandnirana26 <123160499+Nandnirana26@users.noreply.github.com>
2024-10-03 08:03:23 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,25 @@
+const accountId =144553
+let accountEmail="nandni@gamil.com"
+var accountPassword="12345"
+accountCity="Kaithal"//not to use but possible 
+let accountState;
+//declare karke chhod dete h to undefined maan leti h java script kyuki define hi nhi ki aab tak
+
+ 
+// accountId=2//not allowed
+accountEmail="nadni12@gamil.com"
+accountPassword="21212"
+accountCity="Karnal"
+
+console.log(accountId);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+// we use table to make a table in on
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict";//treat all js code as newer version(automaticaally ho jata h)
+
+// alert(2+3)//we are using nodejs, not browser
+
+// we avoid semicolon in react and js most imp is readability
+console.log(3+3);console.log("nandni")//in real word this is bad
+
+// spaces are automatically ignore in js
+// readinility should be high
+// Documentation--->tc3(ecma script(standared leakhe h kaise use karne h) )not easy only for browser, MDN
+
+let name="Nandni"//string
+let age=20//number
+let isLoggedIn=false//boolean
+
+
+// primitives datatypes
+
+//number=> 2 to power 53
+// bigInt=>number badaa ho agar
+// string=>""
+// boolean=>true/false
+// null=>standalone value(type)->special type(representation of empty value)//lhali string alag h defined h string but null is different type
+// undefined=>variable declare but not defined
+// symbol=>when we use react(for uniqueness)
+
+// objects
+
+console.log(typeof "nandni");
+
+console.log(typeof null);//object but undefined is undefined bcz its a type
+
+
+
+
+
+
