02eb9937920efe86ca4bb0d022abf1894913ffb3
Nandnirana26 <123160499+Nandnirana26@users.noreply.github.com>
2024-10-03 10:28:25 +0000

    conversion

---
+++ 01_basic/03_conversionOrOperation.js
@@ -0,0 +1,72 @@
+// let score="33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber)
+
+// // let score=null//it gives the value zero----->"33"=>33
+// // if undefined it gives the NaN
+// // if we give the boolean value true->1,false->0
+// // String ->NaN
+// // "33abc"=>NaN
+
+// let isLoggedIn=1;
+
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// // isLoggedIn=""-->for empty string false and for string true
+// // 1=>true,0=>false
+// // ""=>false
+// // "nandni"=>true
+
+// let someNumber=33;
+
+// let stringNumber=String(someNumber);
+
+// console.log(stringNumber);
+
+// console.log(typeof stringNumber);
+
+// *********************OPERATIONS*******************
+
+let value=3
+let negValue=-value;
+console.log(negValue);
+
+console.log(2**3);//power 2 the power 3
+console.log(2+2);
+console.log(2%4);
+
+let str1="hello";
+let str2=" nandni";
+
+let str3=str1+str2;
+
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(3*4+5%6)//bad code merge hi ni hoge bekaar code
+// if 1st strign ->string
+// if number ->number then string(refer ecma script)
+
+console.log(true);
+console.log(+true);//+conversion
+// console.log(true+);
+console.log(+"");//""->boolean m false but + s vo conversion ho jaayga
+
+let num1,num2,num3
+
+num1=num2=num3=2+2//tricy not used in coding
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter);
+
